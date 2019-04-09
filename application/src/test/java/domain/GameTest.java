b0d178d76ebfb264ceccdd57ed6336f37e1cb69b
Erkka Rahikainen <Erkka.Rahikainen@gmail.com>
2019-04-09 17:45:33 +0300

    Fix tests and jacoco now works

--- application/src/test/java/domain/GameTest.java
+++ application/src/test/java/domain/GameTest.java
@@ -1,25 +1,27 @@
 package domain;
 
+import org.junit.*;
+
+import static junit.framework.TestCase.assertNotNull;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
-
 public class GameTest{
+
+    private Game game;
 
     @Before
     public void setUp(){
-        Game game = new Game("Hello_World2",50);
+        this.game = new Game("Hello_World2",50);
     }
 
     @Test
     public void getGold() {
-        AssertTrue(game.getGold,is(50));
+        assertThat(this.game.getGold(),is(50));
     }
 
-    @org.junit.Test
+    @Test
     public void getWave() {
-        AsserThat(game.getWave,is(0));
+        assertThat(this.game.getWave(),is(0));
     }
 }
